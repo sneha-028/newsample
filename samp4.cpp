654163714b596dd864d4880cfb2e47515c213f04
Sneha Rai <sneharai0206@gmail.com>
2025-07-09 15:09:56 +0530

    Add files via upload

---
+++ samp4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num, rev = 0;
+    cin >> num;
+
+    while (num > 0) {
+        int digit = num % 10;
+        rev = rev * 10 + digit;
+        num = num / 10;
+    }
+
+    cout << "Reversed number: " << rev;
+    return 0;
+}
